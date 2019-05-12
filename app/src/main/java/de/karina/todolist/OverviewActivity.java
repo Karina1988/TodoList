3a1470aa895f99d4ecac14110d2c6d4119aeea16
Karina Ennenga <karina.ennenga@akquinet.de>
2019-05-12 13:16:22 +0200

    refactor in preparation for usage of room

--- app/src/main/java/de/karina/todolist/OverviewActivity.java
+++ app/src/main/java/de/karina/todolist/OverviewActivity.java
@@ -1,10 +1,10 @@
 package de.karina.todolist;
 
 import android.content.Intent;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -18,7 +18,6 @@ import de.karina.todolist.model.TodoItem;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.ToDoubleBiFunction;
 
 public class OverviewActivity extends AppCompatActivity {
 
