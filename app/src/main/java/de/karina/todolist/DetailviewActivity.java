b29234e97ae84a3f4d07bde53300d86d8d0414d3
Karina Ennenga <karina.ennenga@akquinet.de>
2019-05-12 09:28:33 +0200

    add DetailViewActivity

---
+++ app/src/main/java/de/karina/todolist/DetailviewActivity.java
@@ -0,0 +1,40 @@
+package de.karina.todolist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import de.karina.todolist.model.TodoItem;
+
+public class DetailviewActivity extends AppCompatActivity {
+
+    private FloatingActionButton saveButton;
+    private EditText todoTitle;
+    private EditText todoDescription;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailview);
+
+        saveButton = findViewById(R.id.saveButton);
+        saveButton.setOnClickListener((view) -> {
+            saveItem();
+        });
+        
+        todoTitle = findViewById(R.id.todoTitle);
+        todoDescription = findViewById(R.id.todoDescription);
+
+    }
+
+    private void saveItem() {
+        Intent returnIntent = new Intent();
+    
+        TodoItem todoItem = new TodoItem(todoTitle.getText().toString(), todoDescription.getText().toString());
+        
+        returnIntent.putExtra("item", todoItem);
+        setResult(RESULT_OK, returnIntent);
+        finish();
+    }
+}
